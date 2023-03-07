dbaee080193c278627b7ff1bd274d276cb4bd5dd
dibdas <dibyandu08das@gmail.com>
2023-03-07 22:59:58 +0530

    modified

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log(`${+true}`); //1
+console.warn(typeof +true); // Number
+// console.log(++true);// SyntaxError: Invalid left-hand side expression in prefix operation
+
+// opposite  string is boolean value
+console.log(!"anil"); // false
+console.log(typeof "anil");
+console.log(!!"anil"); // true
